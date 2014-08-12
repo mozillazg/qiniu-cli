eb0704c4e1ea90e08ce6df5b6112ce1b93d2d871
Mozillazg <opensource.mozillazg@gmail.com>
2014-08-12 18:18:48 +0800

    增加 --auto-name 选项, 调整配置文件内容

--- qiniu_cli/cli.py
+++ qiniu_cli/cli.py
@@ -15,10 +15,13 @@ from . import __version__
 
 
 class QN(object):
-    def __init__(self, f, bucket_name=None):
+    def __init__(self, f, bucket_name='default'):
         self.config = self.parse_config(f)
         self.qiniu = QiNiu(**self.config['user'])
-        self.bucket = Bucket(self.qiniu, **self.config['bucket'])
+        self.bucket_conf = self.config['bucket'][bucket_name]
+        self.bucket = Bucket(self.qiniu, name=self.bucket_conf['name'],
+                             is_open=self.bucket_conf['is_open'],
+                             domain=self.bucket_conf['domain'])
 
     def parse_config(self, f):
         return json.loads(f.read())
@@ -28,7 +31,7 @@ pass_qiniu = click.make_pass_decorator(QN)
 @click.group()
 @click.option('-c', '--config', required=False, default='config.json',
               type=click.File('rb'), help='Config file(default: config.json).')
-@click.option('--bucket', help='Bucket name.', required=False)
+@click.option('--bucket', help='Bucket name.', required=False, default='default')
 @click.option('-v', '--verbose', is_flag=True,
               help='Enables verbose mode.')
 @click.version_option(__version__)
@@ -43,12 +46,18 @@ def main(ctx, config=None, bucket=None, verbose=False):
 @click.option('--save-name', required=False, help='File save name.')
 @click.option('--save-dir', default='', required=False,
               help='Upload to directory.')
+@click.option('--auto-name', is_flag=True,
+              help='Auto name file by sh1 hex digest with timestamp.')
 @click.argument('f', type=click.File('rb'))
 @pass_qiniu
-def upload(qn, f, save_dir, save_name):
-    if not save_name:
+def upload(qn, f, save_dir, save_name, auto_name):
+    if not save_name and (auto_name or qn.bucket_conf['auto_name']):
         save_name = '%s%s' % (sha1(str(time.time())).hexdigest(),
                               os.path.splitext(f.name)[-1])
+    else:
+        raise click.UsageError('Please set filename with --save-name, '
+                               'or use --auto-name auto name file')
+
     to = os.path.join(save_dir, save_name)
     if not qn.config['upload']['overwrite'] and qn.bucket.exists(to):
         print(qn.bucket.file_url(to))
@@ -56,7 +65,7 @@ def upload(qn, f, save_dir, save_name):
         print(qn.bucket.upload(f, to)['url'])
 
 
-@main.command(short_help='search file.')
+@main.command(short_help='Search file.')
 @pass_qiniu
 def search(qn):
     pass
